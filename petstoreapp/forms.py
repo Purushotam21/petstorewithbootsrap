7b9d3993337266cf418178d16ee8e1936f127fc6
Purushotam kumar <purushotamkumar.s2101@gmail.com>
2024-03-01 19:28:54 +0530

    first commit

---
+++ petstoreapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Pet
+
+class PetForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name','breed','price','image']  
+        
+        
+
+
+
+    
